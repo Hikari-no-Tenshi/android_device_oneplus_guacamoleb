af1b0759533027e43848de3e3e4394998ca3f0b1
Ashwin R C <ashwin2001achu@gmail.com>
2019-08-25 03:28:15 +0000

    guacamoleb: init from guacamole
    Change-Id: Ib745b526730a2807aa75818b5fdebac5112eb043

--- havoc_guacamoleb.mk
+++ havoc_guacamoleb.mk
@@ -8,17 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from guacamole device
-$(call inherit-product, device/oneplus/guacamole/device.mk)
+# Inherit from guacamoleb device
+$(call inherit-product, device/oneplus/guacamoleb/device.mk)
 
 # Inherit some common Havoc stuff.
 $(call inherit-product, vendor/havoc/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_guacamole
-PRODUCT_DEVICE := guacamole
+PRODUCT_NAME := havoc_guacamoleb
+PRODUCT_DEVICE := guacamoleb
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := OnePlus 7 Pro
+PRODUCT_MODEL := OnePlus 7
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_AAPT_CONFIG := xxxhdpi
@@ -26,20 +26,14 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
 
 # Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:9/PKQ1.190110.001/1907281556:user/release-keys"
+BUILD_FINGERPRINT := "OnePlus/OnePlus7/OnePlus7:9/PKQ1.190110.001/1907280700:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=OnePlus7Pro \
-    PRODUCT_NAME=OnePlus7Pro \
-    PRIVATE_BUILD_DESC="OnePlus7Pro-user 9 PKQ1.190110.001 1907281556 release-keys"
+    TARGET_DEVICE=OnePlus7 \
+    PRODUCT_NAME=OnePlus7 \
+    PRIVATE_BUILD_DESC="OnePlus7-user 9 PKQ1.190110.001 1907280700 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
-
-# Havoc
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.havoc.maintainer=SKULSHADY
-
-export export HAVOC_BUILD_TYPE=Official
