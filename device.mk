4db02d629e17cfa60bae206d5ae76aea686f4a91
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2019-10-31 12:01:18 +0200

    guacamoleb: Clean up camera blobs

--- device.mk
+++ device.mk
@@ -17,6 +17,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
     libxml2
 
 # Device init scripts
